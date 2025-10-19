9f57273454b812b4bf276e4839279b4fb891f05f
rakasehan <rakasehan@gmail.com>
2025-10-19 11:41:03 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-sm text-center italic'>  
+      <p>@copy 2025 CuyFace ramalan aing</p>
+    </footer>
+      
+    
+  )
+}
+
+export default Footer
